4805452c127c3e12a14af6614f5a77885326fb02
0x7000 <rekreb81@gmail.com>
2021-04-03 12:00:44 +0300

    fak fak

---
+++ 034_ebob_mat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import math
+
+
+def main():
+    ebob = math.gcd(36, 48)
+    # greatest common divisor, en büyük ortak bölen
+    print(ebob)
+
+
+if __name__ == '__main__':
+    main()
